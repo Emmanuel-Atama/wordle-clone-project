7f3bb2118130f3e2de9290883de0c78a454d54df
Emmanuel Atama <emmanuelatama@outlook.com>
2022-07-14 14:11:39 +0100

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import { useEffect, useState } from "react";
 import Wordle from "./components/Wordle";
+import data from "./data/db.json"
 
 function App() {
   const [solution, setSolution] = useState(null)
+
+  console.log("Inside state: ", data)
   
+  // useEffect(() => {
+  //   fetch('http://localhost:3001/solutions')
+  //     .then(res => res.json())
+  //     .then(json => {
+  //       // random int between 0 & 14
+  //       const randomSolution = json[Math.floor(Math.random()*json.length)]
+  //       setSolution(randomSolution.word)
+  //     })
+  // }, [setSolution])
   useEffect(() => {
-    fetch('http://localhost:3001/solutions')
-      .then(res => res.json())
-      .then(json => {
-        // random int between 0 & 14
-        const randomSolution = json[Math.floor(Math.random()*json.length)]
-        setSolution(randomSolution.word)
-      })
-  }, [setSolution])
+    const randomSolution = data.solutions[Math.floor(Math.random()*data.solutions.length)]
+  setSolution(randomSolution.word)
+  }, [])
 
   return (
     <div className="App">
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from "react";
-
+import data from "../data/db.json"
 export default function Keypad({ usedKeys }) {
     const [letters, setLetters] = useState(null)
   
     useEffect(() => {
-      fetch('http://localhost:3001/letters')
-        .then(res => res.json())
-        .then(json => {
-          setLetters(json)
-        })
+      // fetch('http://localhost:3001/letters')
+      //   .then(res => res.json())
+      //   .then(json => {
+      //     setLetters(json)
+        // })
+        setLetters(data.letters)
     }, [])
   
     return (
